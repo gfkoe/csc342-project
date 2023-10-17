9780617dfc4574e17cce6dd29d242d233a246488
Gabe Koeb <gfkoeb@ncsu.edu>
2023-10-17 11:09:26 -0400

    working on path

--- Milestone1/api/src/routes/TeamRoute.js
+++ Milestone1/api/src/routes/TeamRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const teamRouter = express.Router();
-const TeamDAO = require("db/TeamDAO.js");
+const TeamDAO = require("/db/TeamDAO.js");
 
 module.exports = teamRouter;
