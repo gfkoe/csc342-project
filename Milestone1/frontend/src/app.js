bae65800051b9461426b8b5281c456887d0ae088
Gabe Koeb <gfkoeb@ncsu.edu>
2023-10-20 19:59:44 -0400

    refactored frontend using routes

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
+const routes = express.Router();
 const app = express();
 const PORT = process.env.PORT;
 
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + "/static"));
+
+const frontendRouter = require("./frontendRoutes");
+
+app.use(frontendRouter);
 
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
