c205d0e29f20a1bfe790add3970afc392b94ebe6
Gabe Koeb <gfkoeb@ncsu.edu>
2023-10-20 20:01:07 -0400

    update app

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const app = express();
 const PORT = process.env.PORT;
-
+app.use(express.urlencoded({ extended: true }));
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + "/static"));
 
