a9b329f55d8f76594283badf87c3616235403f63
Gabe Koeb <gfkoeb@ncsu.edu>
2023-11-06 20:48:57 -0500

    edit gitignore and moved around some route stuff

--- Milestone2/api/src/routes/UserRoute.js
+++ Milestone2/api/src/routes/UserRoute.js
@@ -1,22 +1,55 @@
-const express = require('express');
-const frontendRouter = express.Router();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRouter = express.Router();
 
-const path = require('path');
-frontendRouter.use(express.static('static'));
-frontendRouter.use(express.urlencoded({extended: true}));
-const html_dir = path.join(__dirname, '../../templates/');
+const path = require("path");
+// frontendRouter.use(express.static('static'));
+// frontendRouter.use(express.urlencoded({extended: true}));
+// const html_dir = path.join(__dirname, '../../templates/');
 
-frontendRouter.get('/', (req, res) => {
+frontendRouter.get("/", (req, res) => {
   res.sendFile(`${html_dir}index.html`);
 });
 
-
-frontendRouter.get('/error', (req, res) => {
+frontendRouter.get("/error", (req, res) => {
   res.sendFile(`${html_dir}error.html`);
 });
 
-frontendRouter.get('/login', (req,  res) => {
+frontendRouter.get("/login", (req, res) => {
   res.sendFile(`${html_dir}login.html`);
 });
 
-module.exports = frontendRouter;
+const UserDAO = require("./db/UserDAO");
+
+userRouter.post("/users/login", (req, res) => {
+  if (req.body.username && req.body.password) {
+    UserDAO.getUserByCredentials(req.body.username, req.body.password)
+      .then((user) => {
+        let result = {
+          user: user,
+        };
+
+        generateToken(req, res, user);
+
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(err.code).json({ error: err.message });
+      });
+  } else {
+    res.status(401).json({ error: "Not authenticated" });
+  }
+});
+
+userRouter.post("/users/logout", (req, res) => {
+  removeToken(req, res);
+
+  res.json({ success: true });
+});
+
+userRouter.get("/users/current", TokenMiddleware, (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = userRouter;
--- Milestone2/frontend/src/static/js/APIClient.js
+++ Milestone2/frontend/src/static/js/APIClient.js
@@ -1,10 +1,23 @@
-class HTTPClient {
-  static get(url) {
-    return fetch(url).then((res) => {
-      if (!res.ok) {
-        throw new Error("error in request");
-      }
-      return res.json();
-    });
-  }
-}
+import HTTPClient from "./HTTPClient";
+const API_BASE = "api";
+
+export default {
+  getCurrentUser: () => {
+    return HTTPClient.get(API_BASE + "/users/current");
+  },
+  getTeam: (teamId) => {
+    return HTTPClient.get(API_BASE + `/leagues/${leagueId}` + "/schedule");
+  },
+
+  logIn: (username, password) => {
+    let data = {
+      username: username,
+      password: password,
+    };
+    return HTTPClient.post(API_BASE + "/users/login", data);
+  },
+
+  logOut: () => {
+    return HTTPClient.post(API_BASE + "/users/logout", {});
+  },
+};
