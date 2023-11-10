34ca8077e59c1ad10a181630e9357ab2ff219dc6
Gabe Koeb <gfkoeb@ncsu.edu>
2023-11-09 20:25:35 -0500

    added jsonwebtoken and middleware update

--- Milestone2/api/src/middleware/TokenMiddleware.js
+++ Milestone2/api/src/middleware/TokenMiddleware.js
@@ -1,40 +1,39 @@
-const jwt = require('jsonwebtoken');
-
-const JWT_SECRET = 'urmomhahaha'; 
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
+const API_SECRET = process.env.API_SECRET;
 
 // Generate JWT
 function generateToken(user) {
   const payload = {
     user: user,
-    iat: Date.now(), 
-    exp: Math.floor(Date.now() / 1000) + (60 * 60)
+    iat: Date.now(),
+    exp: Math.floor(Date.now() / 1000) + 60 * 60,
   };
 
   return jwt.sign(payload, JWT_SECRET);
 }
 
-// Verify JWT 
+// Verify JWT
 function verifyToken(token) {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     return decoded;
-
   } catch (err) {
-    throw new Error('Invalid token');
+    throw new Error("Invalid token");
   }
 }
 
 // Middleware to authenticate request
 exports.authenticateToken = (req, res, next) => {
-
   // Get auth header
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({error: 'No token provided'});
+    return res.status(401).json({ error: "No token provided" });
   }
-  
+
   // Split into type and token
-  const [type, token] = authHeader.split(' ');
+  const [type, token] = authHeader.split(" ");
 
   // Validate token
   try {
@@ -42,15 +41,12 @@ exports.authenticateToken = (req, res, next) => {
 
     req.user = decoded.user;
     next();
-
   } catch (err) {
-    return res.status(401).json({error: 'Invalid token'});
+    return res.status(401).json({ error: "Invalid token" });
   }
-
 };
 
 // Generate new token
 exports.generateNewToken = (user) => {
   return generateToken(user);
 };
-
