08a7fe7bf8d69af9cc9ab9bd4e62d61304f9fe32
Gabe Koeb <gfkoeb@ncsu.edu>
2023-11-10 16:02:55 -0500

    working on user db

--- Milestone2/api/src/db/UserDAO.js
+++ Milestone2/api/src/db/UserDAO.js
@@ -1,60 +1,35 @@
-const crypto = require("crypto");
+const db = require("./DBConnection");
+const User = require("./models/User");
 
-let users = require("./data/users.json");
-
-//This file mimics making asynchronous request to a database
-module.exports = {
-  getUserByCredentials: (username, password) => {
-    return new Promise((resolve, reject) => {
-      const user = users.find((user) => user.username == username);
+function getUserByCredentials(username, password) {
+  return db
+    .query("SELECT * FROM user WHERE usr_username=?", [username])
+    .then(({ results }) => {
+      const user = new User(results[0]);
       if (user) {
         // we found our user
-        crypto.pbkdf2(
-          password,
-          user.salt,
-          100000,
-          64,
-          "sha512",
-          (err, derivedKey) => {
-            if (err) {
-              //problem computing digest, like hash function not available
-              reject({ code: 400, message: "Error: " + err });
-            }
-
-            const digest = derivedKey.toString("hex");
-            if (user.password == digest) {
-              resolve(getFilteredUser(user));
-            } else {
-              reject({ code: 401, message: "Invalid username or password" });
-            }
-          }
-        );
+        return user.validatePassword(password);
       } else {
         // if no user with provided username
-        reject({ code: 401, message: "No such user" });
+        throw new Error("No such user");
       }
     });
-  },
-
-  getUserById: (userId) => {
-    return new Promise((resolve, reject) => {
-      const user = users.find((user) => user.id == userId);
-      if (user) {
-        console.log(user);
-        resolve(getFilteredUser(user));
-      } else {
-        reject({ code: 401, message: "No such user" });
-      }
-    });
-  },
-};
-
-function getFilteredUser(user) {
-  return {
-    id: user.id,
-    first_name: user.first_name,
-    last_name: user.last_name,
-    username: user.username,
-    avatar: user.avatar,
-  };
 }
+
+function getUserById(userId) {
+  return db
+    .query("SELECT * FROM user WHERE usr_id=?", [userId])
+    .then(({ result }) => {
+      const user = new User(results[0]);
+      if (user) {
+        return user;
+      } else {
+        throw new Error("No such user");
+      }
+    });
+}
+
+module.exports = {
+  getUserByCredentials: getUserByCredentials,
+  getuserById: getUserById,
+};
