f72104b10a8f299fccc01b57ec027197121dd999
Gabe Koeb <gfkoeb@ncsu.edu>
2023-12-09 17:18:09 -0500

    fixed error statement and return

--- FinalProject/api/src/routes/TeamRoute.js
+++ FinalProject/api/src/routes/TeamRoute.js
@@ -8,34 +8,40 @@ const TeamDAO = require("../db/TeamDAO.js");
 teamRouter.get("/teams/:teamName", (req, res) => {
   let team_name = req.params.teamName;
   //
-  TeamDAO.getTeamByName(team_name).then(team => {
-    res.json(team);
-  }).catch(error => {
-    res.status(400).json({error: err});
-  });
+  TeamDAO.getTeamByName(team_name)
+    .then((team) => {
+      res.json(team);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: error });
+    });
 });
 
 // Endpoint to get a team by id
 teamRouter.get("/teams/:teamId", (req, res) => {
   //
   let team_id = req.params.teamId;
-  TeamDAO.getTeamByID(team_id).then(team => {
-    res.json(team);
-  }).catch(error => {
-    res.status(400).json({error: err});
-  });
+  TeamDAO.getTeamByID(team_id)
+    .then((team) => {
+      res.json(team);
+    })
+    .catch((error) => {
+      res.status(400).json({ error: err });
+    });
 });
 
 teamRouter.put("/teams/logo/:teamName", (req, res) => {
   let team_name = req.params.teamName;
   let logo = req.body;
 
-  TeamDAO.addTeamLogo(team_name, logo).then(res => {
-    console.log(res);
-    return;
-  }).catch(error => {
-    res.status(400).json({error: err});
-  });
+  TeamDAO.addTeamLogo(team_name, logo)
+    .then((res) => {
+      console.log(res);
+      return;
+    })
+    .catch((error) => {
+      res.status(400).json({ error: err });
+    });
 });
 
 module.exports = teamRouter;
